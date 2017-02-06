cb2bd5c64261d04a94260004d4b4bb73f9ef70d0
ntwcklng <ntwcklng@gmail.com>
2017-02-06 19:36:46 +0100

    remove ability to add deps

--- bin/nn.js
+++ bin/nn.js
@@ -2,20 +2,13 @@
 import args from 'args'
 import nn from '../lib/'
 
-// const argv = parseArgs(process.argv.slice(2), {
-//   alias: {
-//     h: 'help',
-//     i: 'install',
-//     s: 'silent'
-//   },
-//   boolean: ['h', 'install', 'force', 'init', 'silent', 'debug']
-// })
 args
   .option('install', 'Install all dependencies', false)
   .option('silent', 'ssshhhhhhh!', false)
   .option('init', 'Initialize a Next Project in the current folder', false)
   .option('force', 'Force init', false)
   .option('debug', 'Debugging', false)
+
 const flags = args.parse(process.argv)
 
 try {
