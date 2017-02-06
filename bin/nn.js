1e19fcc1c67aef0a86fc517a03cd78def96ca3e4
ntwcklng <ntwcklng@gmail.com>
2017-02-06 21:16:44 +0100

    short flag for init

--- bin/nn.js
+++ bin/nn.js
@@ -5,11 +5,11 @@ import nn from '../lib/'
 args
   .option('install', 'Install all dependencies', false)
   .option('silent', 'ssshhhhhhh!', false)
-  .option('init', 'Initialize a Next Project in the current folder', false)
+  .option(['t', 'init'], 'Initialize a Next Project in the current folder', false)
   .option('force', 'Force init', false)
   .option('debug', 'Debugging', false)
 
-const flags = args.parse(process.argv)
+const flags = args.parse(process.argv, {value: '<projectName>'})
 
 try {
   nn(flags, args.sub[0])
