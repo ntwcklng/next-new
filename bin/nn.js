84479d626012347507d61729957b7b1a572cbb36
ntwcklng <ntwcklng@gmail.com>
2017-04-15 09:23:19 +0200

    prettier

--- bin/nn.js
+++ bin/nn.js
@@ -1,19 +1,19 @@
 #!/usr/bin/env node
-const path = require('path');
-const args = require('args');
-const updateNotifier = require('update-notifier');
-const asyncToGen = require('async-to-gen/register');
-const isAsyncSupported = require('is-async-supported');
-const nodeVersion = require('node-version');
-const pkg = require('../package.json');
+const path = require('path')
+const args = require('args')
+const updateNotifier = require('update-notifier')
+const asyncToGen = require('async-to-gen/register')
+const isAsyncSupported = require('is-async-supported')
+const nodeVersion = require('node-version')
+const pkg = require('../package.json')
 
-updateNotifier({ pkg }).notify();
+updateNotifier({ pkg }).notify()
 
 if (nodeVersion.major < 6) {
   console.error(
     `Error! next-new requires at least version 6 of Node. Please upgrade!`
-  );
-  process.exit(1);
+  )
+  process.exit(1)
 }
 
 args
@@ -25,23 +25,23 @@ args
   .example(
     'nn my-project --install',
     'Creates a new Next.js Project and installs all dependencies'
-  );
+  )
 
 const flags = args.parse(process.argv, {
   value: '<projectName>',
   mainColor: 'blue',
   subColor: ['dim', 'gray']
-});
+})
 
 if (!isAsyncSupported()) {
   // Support for keywords "async" and "await"
-  const pathSep = process.platform === 'win32' ? '\\\\' : '/';
-  const directoryName = path.parse(path.join(__dirname, '..')).base;
+  const pathSep = process.platform === 'win32' ? '\\\\' : '/'
+  const directoryName = path.parse(path.join(__dirname, '..')).base
 
   asyncToGen({
     includes: new RegExp(`.*${directoryName}?${pathSep}(lib|bin).*`),
     excludes: null,
     sourceMaps: false
-  });
+  })
 }
-require('../lib/')(flags, args.sub[0]);
+require('../lib/')(flags, args.sub[0])
